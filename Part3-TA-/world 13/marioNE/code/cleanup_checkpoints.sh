322234f7f1b1786c39daa213657efcf11639297b
s4nyam <sanyamlala@gmail.com>
2023-10-19 23:41:16 +0200

    adding experiments

--- Part3-TA-/world 13/marioNE/code/cleanup_checkpoints.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#ls -1tp | grep -E "neat-checkpoint-parallel-[0-9]" | tail -n +5 | xargs -I {} rm -- {}
-ls -1tp | grep -E "neat-checkpoint-[0-9]" | tail -n +5 | xargs -I {} rm -- {}
